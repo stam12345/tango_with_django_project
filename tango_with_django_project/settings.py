90e05727d399c3f2b122e4e4919e7c8fe064815e
Stamatis Theocharous <2380138T@student.gla.ac.uk>
2021-02-03 16:55:23 +0000

    Chapter9

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -99,6 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS' : { 'min_length' : 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -134,3 +135,7 @@ STATICFILES_DIRS = [STATIC_DIR, ]
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+
+
+#lOGin
+LOGIN_URL= 'rango:login'
